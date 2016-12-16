8299078469c5564ff941473ec41ec7f7ca2e812a
Dalas <kramarenko.yurii.viktorovich@gmail.com>
2016-12-16 15:16:19 +0200

    Add shoppingCart reducer

---
+++ src/reducers/ShopingCartReducer.js
@@ -0,0 +1,30 @@
+/**
+ * Created by yura on 16.12.16.
+ */
+
+import * as actionTypes from '../actions/ActionTypes';
+
+
+const initialState = {
+    products: {}
+};
+
+export default function shopingCartReducer(state = initialState, action) {
+    switch( action.type ) {
+        case actionTypes.ADD_TO_CART:
+            state.products[ action.product.id ] = action.product;
+            return state;
+
+        case actionTypes.REMOVE_FROM_CART:
+            delete state.products[ action.product.id ];
+            return state;
+
+        case actionTypes.INCREMENT_PRODUCT:
+            state.products[ action.id ].count += 1;
+            return state;
+
+        case actionTypes.DECREMENT_PRODUCT:
+            state.products[ action.id ].count -= 1;
+            return state;
+    }
+}
