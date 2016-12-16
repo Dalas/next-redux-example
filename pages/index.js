e1821a0ae8b219833e62bfba1c2aa75a803621ef
Dalas <kramarenko.yurii.viktorovich@gmail.com>
2016-12-16 17:41:38 +0200

    Added font-awesome

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,12 @@
 import React from 'react'
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import store from '../src/store/Store';
 import ProductList from '../src/containers/ProductListContainer';
+import ShoppingCart from '../src/components/ShoppingCartComponent';
 
 
 export default class extends React.Component {
-    state = {
-        products: [
-            {
-                title: 'P1',
-                imgUrl: '/static/product1.png',
-                description: 'Some description',
-                cost: 10
-            },
-            {
-                title: 'P2',
-                imgUrl: '/static/product2.png',
-                description: 'Some description',
-                cost: 12
-            },
-            {
-                title: 'P3',
-                imgUrl: '/static/product3.png',
-                description: 'Some description',
-                cost: 20
-            }
-        ]
-    };
     constructor(props) {
         super(props);
     }
@@ -40,9 +20,27 @@ export default class extends React.Component {
 
     render() {
         return (
-            <Provider store={ store }>
-                <ProductList />
-            </Provider>
+            <div>
+                <Head>
+                    <title>My page title</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <link rel="stylesheet" href="/static/css/font-awesome.min.css" />
+                </Head>
+
+                <Provider store={ store }>
+                    <ProductList />
+                </Provider>
+                <ShoppingCart />
+
+                <style jsx>{`
+                    div {
+                        display: flex;
+                        justify-content: space-around;
+                        width: 1000px;
+                        margin: 0 auto;
+                    }
+                `}</style>
+            </div>
         )
     }
 }
