db00e49a4fe3e37b6f03522ee9006b16e0feaed2
Dalas <kramarenko.yurii.viktorovich@gmail.com>
2017-01-24 19:49:49 +0200

    Minor changes

--- src/components/GroupsComponent.js
+++ src/components/GroupsComponent.js
@@ -5,20 +5,12 @@
 import React from 'react';
 
 
-export default ({ product, discount, addToCart, removeFromCart }) => {
-    discount = parseFloat(product.cost * discount / 100).toFixed(2);
+export default ( props ) => {
+    console.log(props);
 
     return (
         <div className="productWrapper">
-            <h2>{ product.title }</h2>
-            <div className="imageWrapper">
-                <img src={ product.imgUrl } />
-            </div>
-            <p>Cost: <span className={ discount > 0 ? 'red' : '' }>${ parseFloat(product.cost).toFixed(2) }</span><span className={ discount > 0 ? 'green' : 'hidden' }> ${ parseFloat(product.cost - discount).toFixed(2) } </span></p>
-            <p>Available count: { product.availableCount }</p>
-            <p>{ product.description }</p>
-
-            <button className={ product.addedToCart ? 'remove' : 'add' } onClick={ product.addedToCart ? removeFromCart : addToCart } >{ product.added ? 'Remove from cart!' : 'Add to cart!' }</button>
+            asd
         </div>
     )
 }
--- src/components/TasksComponent.js
+++ src/components/TasksComponent.js
@@ -2,26 +2,12 @@ import React from 'react';
 import Product from './ProductComponent';
 
 
-export default ({ products, discount, actions }) => {
+export default ( props ) => {
+    console.log( props );
+
     return (
         <div className="products-container">
-            { products.map( (product, index) =>
-                <Product
-                    product={ product }
-                    discount={ discount }
-                    key={ index }
-                    addToCart={() => actions.addToCart( product ) }
-                    removeFromCart={() => actions.removeFromCart( product )}
-                />
-            ) }
-
-            <style jsx>{`
-                .products-container {
-                    display: inline-block;
-                    width: 700px;
-                    text-align: justify;
-                }
-            `}</style>
+            asdasdasdasd
         </div>
     )
 }
